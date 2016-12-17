a761c3b44ead12b4557fc2bad8db05edf7572131
David-Estevez <david.estevez.fdez@gmail.com>
2016-12-17 04:03:27 +0100

    Add team info retrieval

---
+++ advanced.py
@@ -0,0 +1,51 @@
+import requests
+import json
+from tqdm import tqdm
+import begin
+
+
+class DataRetriever(object):
+    def __init__(self, token=None):
+        self.cache = {}
+        if token:
+            self.params = {'access_token': token}
+        else:
+            self.params = None
+
+    def retrieve(self, url):
+        if url not in self.cache:
+            resp = requests.get(url=url, params=self.params)
+            data = json.loads(resp.text)
+            self.cache[url] = data
+
+        return self.cache[url]
+
+def do_stuff(*args, **kwargs):
+    url_org = 'https://api.github.com/orgs/{}'
+    dr = DataRetriever(kwargs['access_token'])
+    asrob = {}
+
+    # Teams
+    url_teams = url_org.format(kwargs['org'])+'/teams'
+    teams_data = dr.retrieve(url_teams)
+
+    asrob['teams'] = dict()
+    for team in teams_data:
+        work_group = dict()
+        work_group['members'] = []
+        for member in tqdm(dr.retrieve(team['members_url'].replace('{/member}', ''))):
+            work_group['members'].append(member['login'])
+        work_group['repos'] = []
+        for repo in tqdm(dr.retrieve(team['repositories_url'])):
+            work_group['repos'].append(repo['name'])
+        asrob['teams'][team['name']] = work_group
+
+    asrob['repos'] = dict()
+
+    
+    print(asrob)
+
+
+@begin.start(auto_convert=True)
+def main(org, access_token = None):
+    return do_stuff(org=org, access_token=access_token)
